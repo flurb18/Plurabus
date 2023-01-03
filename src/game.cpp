aefbcc3b7e1455ebf395fd16b6d5c49c8a90925a
BuildTools <unconfigured@null.spigotmc.org>
2023-01-03 14:19:16 -0500

    Bugfix

--- src/game.cpp
+++ src/game.cpp
@@ -1043,11 +1043,12 @@ void Game::mainLoop(void) {
     disp->fillBlack();
     disp->drawText("Connecting...",0,0);
   } else {
-    pthread_mutex_lock(&threadLock);
-    draw();
-    SDL_Event e;
-    if (SDL_PollEvent(&e) != 0) handleSDLEvent(&e);
-    pthread_mutex_unlock(&threadLock);
+    if (pthread_mutex_trylock(&threadLock) == 0) {
+      draw();
+      SDL_Event e;
+      if (SDL_PollEvent(&e) != 0) handleSDLEvent(&e);
+      pthread_mutex_unlock(&threadLock);
+    }
   }
   disp->update();
 }
