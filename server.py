7760c562005ed501362aea199ba3b2de31ba5e07
BuildTools <unconfigured@null.spigotmc.org>
2023-03-19 16:13:47 -0400

    Cleaning

--- server.py
+++ server.py
@@ -12,11 +12,17 @@ import pathlib
 import uuid
 import sys
 import secrets
-import json
 from google.cloud import recaptchaenterprise_v1
 from google.cloud.recaptchaenterprise_v1 import Assessment
 
-CONTENT_TYPES = {
+BlockedDirectPages = [ "play.html", "private.html" ]
+StatusPages = {
+    'not-found' : (http.HTTPStatus.NOT_FOUND, {}, b"Not found\n"),
+    'too-long' : (http.HTTPStatus.REQUEST_URI_TOO_LONG, {}, b"Request URI too long\n"),
+    'bad-req-queries' : (http.HTTPStatus.BAD_REQUEST, {}, b"Bad request: missing queries\n"),
+    'bad-req-captcha' : (http.HTTPStatus.BAD_REQUEST, {}, b"Bad request: failed captcha\n")
+}
+ContentTypes = {
     ".css": "text/css",
     ".html": "text/html; charset=utf-8",
     ".ico": "image/x-icon",
@@ -26,44 +32,49 @@ CONTENT_TYPES = {
     ".data": "binary"
 }
 
-if (len(sys.argv) < 2):
-    print("Input host name as command argument")
-    exit()
+RECAPTCHA_SITE_KEY = "6LetnQQlAAAAABNjewyT0QnLyxOPkMharK-SILmD"
+PROJECTID = "skillful-garden-379804"
 
-hostName = sys.argv[1]
+CSP = "script-src 'self' 'wasm-unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;"
+CSP += "img-src 'self' blob:;"
+CSP += "frame-src 'self' https://www.google.com;"
+CSP += "connect-src 'self';"
+CSP += "default-src 'self';"
 
-recaptchaSiteKey = "6LetnQQlAAAAABNjewyT0QnLyxOPkMharK-SILmD"
-projectID = "skillful-garden-379804"
-
-BlockedDirectPages = [ "play.html", "private.html" ]
-StatusPages = {
-    'not-found' : (http.HTTPStatus.NOT_FOUND, {}, b"Not found\n"),
-    'too-long' : (http.HTTPStatus.REQUEST_URI_TOO_LONG, {}, b"Request URI too long\n"),
-    'bad-req-queries' : (http.HTTPStatus.BAD_REQUEST, {}, b"Bad request: missing queries\n"),
-    'bad-req-captcha' : (http.HTTPStatus.BAD_REQUEST, {}, b"Bad request: failed captcha\n")
-}
+FRAME_DELAY = 0.010
+TOKEN_LIFETIME = 15
+LOBBY_KEY_LIFETIME = 180
 
 Tokens = []
 SocketQueue = []
 LobbyKeys = []
-FRAME_DELAY = 0.010
 
-def expire_token(token):
-    if token in Tokens:
-        Tokens.remove(token)
+TokenLock = asyncio.Lock()
 
-def create_token(lifetime=15):
+async def create_token():
     token = uuid.uuid4().hex
+    await TokenLock.acquire()
     Tokens.append(token)
-    asyncio.get_running_loop().call_later(lifetime, expire_token, token)
+    TokenLock.release()
+    asyncio.ensure_future(remove_token_later(token))
     return token
 
-def create_lobby_key(lifetime=180):
+def create_lobby_key():
     lobbyKey = secrets.token_urlsafe(12)
     LobbyKeys.append(lobbyKey)
-    asyncio.get_running_loop().call_later(lifetime, LobbyKeys.remove, lobbyKey)
+    asyncio.get_running_loop().call_later(LOBBY_KEY_LIFETIME, LobbyKeys.remove, lobbyKey)
     return lobbyKey
 
+async def remove_token(token):
+    await TokenLock.acquire()
+    if token in Tokens:
+        Tokens.remove(token)
+    TokenLock.release()
+
+async def remove_token_later(token):
+    await asyncio.sleep(TOKEN_LIFETIME)
+    await remove_token(token)
+    
 # From https://cloud.google.com/recaptcha-enterprise/docs/create-assessment
 def create_assessment(project_id, recaptcha_site_key, token):
     client = recaptchaenterprise_v1.RecaptchaEnterpriseServiceClient()
@@ -77,6 +88,20 @@ def create_assessment(project_id, recaptcha_site_key, token):
     request.parent = f"projects/{project_id}"
     response = client.create_assessment(request)
     return response
+
+async def noop_handler(websocket):
+    pass
+
+async def timerLoop(websocket):
+    while (True):
+        await asyncio.sleep(1)
+        try:
+            await websocket.send("TIMER");
+            await websocket.pairedClient.send("TIMER");
+        except websockets.exceptions.ConnectionClosed:
+            return
+        except Exception as e:
+            return
 
 async def serve_html(requrl, request_headers):
     parsed_url = urllib.parse.urlparse(requrl)
@@ -101,14 +126,14 @@ async def serve_html(requrl, request_headers):
             return StatusPages['bad-req-queries']
         actionString = queries['a'][0]
         recaptchaToken = queries['t'][0]
-        assessment = create_assessment(projectID, recaptchaSiteKey, recaptchaToken)
+        assessment = create_assessment(PROJECTID, RECAPTCHA_SITE_KEY, recaptchaToken)
         if (not assessment.token_properties.valid or
             assessment.token_properties.action != actionString or
             assessment.risk_analysis.score < 0.5):
             return StatusPages['bad-req-captcha']
         if (actionString == "public"):
             page = "play.html"
-            token = create_token()
+            token = await create_token()
         elif (actionString == "private"):
             page = "private.html"
             lobbyKey = create_lobby_key()
@@ -117,7 +142,7 @@ async def serve_html(requrl, request_headers):
     elif (page in LobbyKeys):
         pstr = page
         page = "play.html"
-        token = create_token()
+        token = await create_token()
     try:
         p = pathlib.Path(__file__).resolve()
         template = p.parent.joinpath("web").joinpath(page)
@@ -125,14 +150,9 @@ async def serve_html(requrl, request_headers):
         pass
     else:
         if template.is_file():
-            csp = "script-src 'self' 'wasm-unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;"
-            csp += "img-src 'self' blob:;"
-            csp += "frame-src 'self' https://www.google.com;"
-            csp += "connect-src 'self';"
-            csp += "default-src 'self';"
             headers = {
-                "Content-Type": CONTENT_TYPES[template.suffix],
-                "Content-Security-Policy": csp
+                "Content-Type": ContentTypes[template.suffix],
+                "Content-Security-Policy": CSP
             }
             body = template.read_bytes()
             if (template.name == 'private.html'):
@@ -143,20 +163,6 @@ async def serve_html(requrl, request_headers):
 
     return StatusPages['not-found']
 
-async def noop_handler(websocket):
-    pass
-
-async def timerLoop(websocket):
-    while (True):
-        await asyncio.sleep(1)
-        try:
-            await websocket.send("TIMER");
-            await websocket.pairedClient.send("TIMER");
-        except websockets.exceptions.ConnectionClosed:
-            return
-        except Exception as e:
-            return
-
 async def serve_websocket(websocket, path):
     try:
         token = await websocket.recv()
@@ -166,7 +172,7 @@ async def serve_websocket(websocket, path):
         await websocket.close(1011, "Invalid token")
         return
     else:
-        expire_token(token)
+        await remove_token(token)
     websocket.desiredPairedString = await websocket.recv()
     websocket.foundPartner = False
     websocket.readyForGame = asyncio.Event()
