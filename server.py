6edf308bb1f2bde6fbd40488e90249c29fbeb7a1
BuildTools <unconfigured@null.spigotmc.org>
2023-04-09 04:11:23 -0400

    better logs and I/O

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ import quart_trio
 import hypercorn
 from wsproto.utilities import LocalProtocolError
 import json
+from time import strftime
 from urllib.parse import parse_qs
 import argparse
 import pathlib
@@ -139,9 +140,33 @@ async def create_assessment(project_id, recaptcha_site_key, token):
     response = await trio_asyncio.aio_as_trio(client.create_assessment)(request)
     return response
 
-#-----------------------Dynamic file responder-----------------------
+#---------------------------I/O---------------------------------#
 
-async def serve_dynamic_file(path, textMap, wasm = True):
+async def flush_output_buffer_loop():
+    async with await LogFile.open("a") as f:
+        while(True):
+            await trio.sleep(OUTPUT_BUFFER_FLUSH_INTERVAL)
+            async with OutputBuffer.lock:
+                await f.writelines(OutputBuffer.var)
+                await f.flush()
+                OutputBuffer.var.clear()
+
+async def log(string):
+    async with OutputBuffer.lock:
+        OutputBuffer.var.append(string)
+            
+async def log_exception_group(egrp):
+    for e in egrp.exceptions:
+        await log(f"{str(e)}\n")
+
+def get_prompt(handle, printpaired=True):
+    if hasattr(handle, "pairedClient") and printpaired:
+        prompt = f"[{strftime('%c')} {handle.remote_addr} - {handle.pairedClient.remote_addr}]"
+    else:
+        prompt = f"[{strftime('%c')} {handle.remote_addr}]"
+    return prompt
+
+async def serve_dynamic_file(path, textMap, wasm):
     try:
         template = await ServerRoot.joinpath("web").joinpath(path).resolve()
     except ValueError:
@@ -157,19 +182,23 @@ async def serve_dynamic_file(path, textMap, wasm = True):
     response.headers["Content-Security-Policy"] = WasmCSP if wasm else DefaultCSP
     return response
 
+
 #--------------------Main game Websocket methods--------------------
     
 async def timer_loop(websocket):
     async with SessionGamesPlayed.lock:
         SessionGamesPlayed.var += 1
-    async with OutputBuffer.lock:
-        OutputBuffer.var.append("Game started, paired "+websocket.remote_addr+" with "+websocket.pairedClient.remote_addr+"\n")
-    for _ in range(GAME_LIFETIME):
-        await trio.sleep(1)
-        await websocket.send("TIMER")
-        await websocket.pairedClient.send("TIMER")
-    await websocket.send("TIMEOUT")
-    await websocket.pairedClient.send("TIMEOUT")
+        await log(f"{get_prompt(websocket)} Game started\n")
+    try:
+        for _ in range(GAME_LIFETIME):
+            await trio.sleep(1)
+            await websocket.send("TIMER")
+            await websocket.pairedClient.send("TIMER")
+        await websocket.send("TIMEOUT")
+        await websocket.pairedClient.send("TIMEOUT")
+    finally:
+        with trio.CancelScope(shield=True):
+            await log(f"{get_prompt(websocket)} Game ended\n")
 
 async def game_loop(websocket):
     while (True):
@@ -179,23 +208,23 @@ async def game_loop(websocket):
             await websocket.pairedClient.send(msg)
         if cancel_scope.cancelled_caught:
             await websocket.send("FRAME_TIMEOUT")
+            await log(f"{get_prompt(websocket, printpaired=False)} Frame timeout\n")
             return
     
 async def serve_game_websocket(websocket):
     with trio.move_on_after(STARTUP_TIMEOUT) as cancel_scope:
         firstMessage = await websocket.receive()
-        if not isinstance(firstMessage, str):
-            return
-        if len(firstMessage) != TOKEN_LENGTH:
+        if not isinstance(firstMessage, str) or len(firstMessage) != TOKEN_LENGTH:
+            await log(f"{get_prompt(websocket)} Token malformed\n")
             return
         async with Tokens.lock:
             tokenValid = firstMessage in Tokens.var
             if tokenValid:
                 tokenValid = tokenValid and (Tokens.var.pop(firstMessage) == websocket.remote_addr)
         if not tokenValid:
+            await log(f"{get_prompt(websocket)} Invalid token received\n")
             return
-        async with OutputBuffer.lock:
-            OutputBuffer.var.append("Accepted game connection, remote "+websocket.remote_addr+"\n")
+        await log(f"{get_prompt(websocket)} Accepted game connection\n")
         pairString = await websocket.receive()
         websocket.foundPartner = False
         websocket.readyForGame = trio.Event()
@@ -243,7 +272,6 @@ async def serve_game_websocket(websocket):
             nursery.start_soon(game_loop, websocket)
             if (websocket.player == 0):
                 nursery.start_soon(timer_loop, websocket)
-
     
 #---------------------Route Handlers-------------------------#
 
@@ -257,7 +285,7 @@ async def serve_playercount_websocket():
                 message = "Players Online: " + str(PlayerCount.var)
             await quart.websocket.send(message)
             await trio.sleep(NUMPLAYERS_REFRESH_TIME)
-    except* LocalProtocolError:
+    except LocalProtocolError:
         pass
     finally:
         with trio.CancelScope(shield = True):
@@ -266,14 +294,17 @@ async def serve_playercount_websocket():
 
 @app.websocket("/d/game")
 async def serve_game_websocket_wrapper():
+    handle = quart.websocket._get_current_object()
+    await log(f"{get_prompt(handle)} Incoming connection\n")
     async with PlayerCount.lock:
         PlayerCount.var += 1
     try:
-        await serve_game_websocket(quart.websocket._get_current_object())
-    except* LocalProtocolError:
+        await serve_game_websocket(handle)
+    except LocalProtocolError:
         pass
     finally:
         with trio.CancelScope(shield = True):
+            await log(f"{get_prompt(handle, printpaired=False)} Ended connection\n")
             async with PlayerCount.lock:
                 PlayerCount.var -= 1
 
@@ -303,6 +334,7 @@ async def serve_http_dynamic():
     try:
         postData = await quart.request.get_data()
     except Exception as e:
+        await log(f"Bad POST request\n{str(e)}\n")
         quart.abort(400)
     recaptchaToken = parse_qs(postData.decode("utf-8")).get("recaptcha-token",[""])[0]
     if not args.test:
@@ -315,10 +347,10 @@ async def serve_http_dynamic():
             quart.abort(401, "Failed captcha")
     if (actionString == "public"):
         token = await create_token(quart.request.remote_addr)
-        return await serve_dynamic_file("play.html",{ "TOKEN_PLACEHOLDER" : token, "PSTR_PLACEHOLDER" : PUBLIC_PAIRSTRING })
+        return await serve_dynamic_file("play.html",{ "TOKEN_PLACEHOLDER" : token, "PSTR_PLACEHOLDER" : PUBLIC_PAIRSTRING }, True)
     elif (actionString == "private"):
         lobbyKey = await create_lobby_key()
-        return await serve_dynamic_file("private.html",{ "KEY_PLACEHOLDER" : lobbyKey }, wasm = False)
+        return await serve_dynamic_file("private.html",{ "KEY_PLACEHOLDER" : lobbyKey }, False)
     else:
         quart.abort(404)
             
@@ -330,7 +362,7 @@ async def serve_http_lobbykey(lobbyKey):
         keyValid = lobbyKey in PrivateGames.var
     if keyValid:
         token = await create_token(quart.request.remote_addr)
-        return await serve_dynamic_file("play.html",{ "TOKEN_PLACEHOLDER" : token, "PSTR_PLACEHOLDER" : lobbyKey })
+        return await serve_dynamic_file("play.html",{ "TOKEN_PLACEHOLDER" : token, "PSTR_PLACEHOLDER" : lobbyKey }, True)
     else:
         quart.abort(404)
 
@@ -339,7 +371,7 @@ async def serve_static_file(filePath):
     if not args.test:
         quart.abort(404)
     rewrites = NoCaptchaRewrites if filePath in NoCaptchaRewriteFiles else {}
-    return await serve_dynamic_file("static/" + filePath, rewrites, wasm = False)
+    return await serve_dynamic_file("static/" + filePath, rewrites, False)
 
 @app.route("/", methods=["GET"])
 async def serve_homepage():
@@ -360,22 +392,14 @@ class ProxyMiddleware:
         return await self.app(scope, receive, send)
 
 #-----------------------Main------------------------------------#
-
-async def flush_output_buffer_loop():
-    async with await LogFile.open("a") as f:
-        while(True):
-            await trio.sleep(OUTPUT_BUFFER_FLUSH_INTERVAL)
-            async with OutputBuffer.lock:
-                await f.writelines(OutputBuffer.var)
-                await f.flush()
-                OutputBuffer.var.clear()
-
+            
 async def main_app():
     cfg = hypercorn.config.Config()
     cfg.bind = "unix:"+str(ServerRoot.joinpath("web.sock"))
     cfg.workers = 1
     cfg.websocket_ping_interval = 5.0
     app.asgi_app = ProxyMiddleware(app.asgi_app)
+    await log("Starting server\n")
     await hypercorn.trio.serve(app, cfg)
 
 async def main():
