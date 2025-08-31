16b8153c90161610b9baab6f85941fc370dbd937
flurb18 <flurb18@gmail.com>
2025-08-30 23:50:06 -0400

    asdf

--- web/static/script/index.js
+++ web/static/script/index.js
@@ -1,10 +1,4 @@
-function buttonClick(actionType) {
-    grecaptcha.enterprise.ready(async () => {
-        const token = await grecaptcha.enterprise.execute('6LetnQQlAAAAABNjewyT0QnLyxOPkMharK-SILmD', {action: actionType});
-	document.getElementById(actionType+"forminput").value = token;
-	document.getElementById(actionType+"form").submit();
-    });
-}
+
 document.getElementById('publicformbutton').onclick = function () { buttonClick("public") };
 document.getElementById('privateformbutton').onclick = function () { buttonClick("private") };
 const countersocket = new WebSocket((window.location.origin+"/d/playercount").replace(/^https(.*)/, 'wss$1'));
