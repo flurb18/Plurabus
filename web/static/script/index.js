a7a8ae114143e7ea5afb6cdf020bf670b05b6e5a
flurb18 <flurb18@gmail.com>
2025-09-01 03:18:57 -0400

    pls

--- web/static/script/index.js
+++ web/static/script/index.js
@@ -1,9 +1,3 @@
-function setupButton(name) {
-    document.getElementById(name + "formbutton").onclick = function () { document.getElementById(name + "form").submit() };
-}
-setupButton("public")
-setupButton("private")
-setupButton("practice")
 const countersocket = new WebSocket((window.location.origin+"/d/playercount").replace(/^https(.*)/, 'wss$1'));
 countersocket.onmessage = function(event) {
     document.getElementById("counterDiv").innerText = event.data;
--- web/static/script/practice.js
+++
@@ -1,4 +0,0 @@
-function buttonClick(actionType) {
-	document.getElementById(actionType+"form").submit();
-}
-document.getElementById('spawnerattackformbutton').onclick = function () { buttonClick("spawnerattack") };
